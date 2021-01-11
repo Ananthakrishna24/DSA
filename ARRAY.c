a65ffa58d94e22d6446fd08895176188ea3f6e57
Ananthakrishna S <47441705+Ananthakrishna24@users.noreply.github.com>
2021-01-11 13:42:27 +0530

    Add files via upload

---
+++ ARRAY.c
@@ -0,0 +1,85 @@
+/*
+ Design, Develop and Implement a menu driven Program in C for the following array operations.
+ a. Creating an array of N Integer Elements
+ b. Display of array Elements with Suitable Headings
+ c. Inserting an Element (ELEM) at a given valid Position (POS)
+ d. Deleting an Element at a given valid Position (POS)
+ e. Exit.
+ Support the program with functions for each of the above operations.
+ */
+
+
+
+#include<stdio.h>
+#include<stdlib.h>
+
+int a[10],n,elem,i,pos;
+void create();
+void display();
+void insert();
+void delete();
+
+void create(){
+    int i;
+    printf("=> 1. Enter the size of the array\n");
+    scanf("%d", &n);
+    printf("Enter the elements in the array:\n");
+    for(i=0; i<n; i++){
+        scanf("%d", &a[i]);
+    }
+
+}
+void display()
+{
+    int i;
+    printf("The array elements are:\n");
+    for(i=0; i<n; i++)
+        printf("%d\t",a[i]);
+}
+
+void insert(){
+    int i;
+    printf("=> 1. Enter the position to be inserted\n");
+    scanf("%d", &pos);
+    printf("=> 1. Enter the element\n");
+    scanf("%d", &elem);
+    for(i = n-1; i>=pos; i--){
+        a[i+1] = a[i];
+    }
+    a[pos] = elem;
+    n++;
+}
+
+void delete()
+{
+    printf("Enter  position of element to be deleted:\n");
+    scanf("%d",&pos);
+    elem=a[pos];
+    for(i=pos; i<n; i++){
+        a[i] = a[i+1];
+    }
+    n--;
+    printf("Deleted element is %d\n",elem );
+}
+
+
+int main()
+{
+    int ch;
+    while(ch)
+    {
+        printf("\n\n______MENU_____\n");
+        printf("1.Create\n2.Display\n3.Insert\n4.Delete\n5.Exit\n");
+        printf("Enter Your Choice:");
+        scanf("%d",&ch);
+        switch(ch)
+        {
+            case 1:create();break;
+            case 2:display();break;
+            case 3:insert();break;
+            case 4:delete();break;
+            case 5:exit(0);break;
+            default :printf("INVALID CHOICE\n");
+        }
+    }return 0;
+}
